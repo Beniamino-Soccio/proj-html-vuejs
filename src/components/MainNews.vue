2d0669f0639d5ce259cb7e7a21d6b6028068b3a0
DanieleChecchin <danielechecchin26@gmail.com>
2024-09-25 18:22:20 +0200

    layout news

--- src/components/MainNews.vue
+++ src/components/MainNews.vue
@@ -9,14 +9,28 @@ export default {
 </script>
 
 <template>
-  <h1>MainNews</h1>
-<div>
-news
-</div>
+  <section class="container">
+    <div class="top-news d-flex justify-content-between">
+      <h3>LIFESTYLE & STORIES</h3>
+      <div class="links">
+        <button type="button" class="btn btn-secondary">ALL</button>
+        <button type="button" class="btn btn-secondary mx-5">LIFESTYLE</button>
+        <button type="button" class="btn btn-secondary">STORIES</button>
+      </div>
+    </div>
+    <div class="main-news">
+      <div class="row">
+        <div class="col-8">
+          <img src="../assets/img/success-story.webp" alt="">
+        </div>
+        <div class="col-4 flex">uno</div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
-div{
+section {
   height: 500px;
   border: 1px dashed goldenrod;
 }
