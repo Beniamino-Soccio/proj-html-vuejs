27634b5b6885aa9f6c3c52a02e85d167eb9da456
Beniamino-Soccio <beniamino43@gmail.com>
2024-09-25 15:33:11 +0200

    imported all components

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,20 @@
 <script>
+import MainGallery from "./components/MainGallery.vue";
+import MainNews from "./components/MainNews.vue";
+import MainPost from "./components/MainPost.vue";
+import MainSlider from "./components/MainSlider.vue";
+import MainThumbs from "./components/MainThumbs.vue";
+import MainTrailer from "./components/MainTrailer.vue";
+
 export default {
+  components: {
+    MainGallery,
+    MainNews,
+    MainPost,
+    MainSlider,
+    MainThumbs,
+    MainTrailer,
+  }
   data() {
     return {
       
@@ -9,6 +24,13 @@ export default {
 </script>
 
 <template>
+
+<MainGallery />
+<MainNews />
+<MainPost />
+<MainSlider />
+<MainThumbs />
+<MainTrailer />
 
 </template>
 
