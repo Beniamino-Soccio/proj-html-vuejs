d39cabaa6f1ba007a9b67424ac7112a7d62635cf
abdicore <abdirhamanmohamed@gmail.com>
2024-09-25 16:45:44 +0200

    block-layout

--- src/components/MainGallery.vue
+++ src/components/MainGallery.vue
@@ -9,16 +9,45 @@ export default {
 </script>
 
 <template>
-  <div>
+  <div class="conteiner">
+    <div class="gallery">
+      <div class="row ">
+        <div class="col-3 side">
+        </div>
+        <div class="col-6 central">
+        </div>
+        <div class="col-3 side">
+        </div>
 
-    <h5>Abdi MainGallary</h5>
+
+      </div>
+
+    </div>
+
+  
   </div>
 
 </template>
 
 <style scoped>
-div{
+.gallery{
   height: 500px;
   border: 1px dashed dodgerblue;
+  display: flex;
 }
+
+.central {
+  border-radius: 10px;
+  background-color: gray;
+  width: 400px;
+  height: 450 px,
+}
+.side{
+  border-radius: 10px;
+  background-color: green;
+  width: 100px;
+  height: 200px,
+}
+
+
 </style>
