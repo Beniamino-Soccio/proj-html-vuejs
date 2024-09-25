654b9daba8ac666b44523b8b32e7b7d603223307
Beniamino-Soccio <beniamino43@gmail.com>
2024-09-25 19:54:30 +0200

    added dynamic list of trailers

--- src/components/MainTrailer.vue
+++ src/components/MainTrailer.vue
@@ -2,8 +2,50 @@
 export default {
   data() {
     return {
-      urlVideos:[
-      https://www.youtube.com/embed/9rYkZlt2hHE?si=3dtJ99zCLUHm3fv6,
+      videos:[
+        {
+          url: "https://www.youtube.com/embed/9rYkZlt2hHE?si=3dtJ99zCLUHm3fv6",
+          name: "Eternal Cultivation Anime",
+          img: "https://i.ytimg.com/vi/9rYkZlt2hHE/hqdefault.jpg",
+          index:1
+        },
+        {
+          url: "https://www.youtube.com/embed/zlwQERpksnw?si=wDOqxFA5RRVfJypX",
+          name: "Sun's Out, Guns Out",
+          img: "https://i.ytimg.com/vi/zlwQERpksnw/hqdefault.jpg",
+          index:2
+        },
+        {
+          url: "https://www.youtube.com/embed/itQnUDMMhaU?si=9PYUEuQkEzBVg5pQ",
+          name: "Top 10 Vinland Saga Moments",
+          img: "https://i.ytimg.com/vi/itQnUDMMhaU/hqdefault.jpg",
+          index:3
+        },
+        {
+          url: "https://www.youtube.com/embed/IrU2dklT11s?si=niklH5vQLpJrQ2Vf",
+          name: "Everytime Sukuna Appears",
+          img: "https://i.ytimg.com/vi/IrU2dklT11s/hqdefault.jpg",
+          index:4
+        },
+        {
+          url: "https://www.youtube.com/embed/H5qupR6YQpQ?si=kfkvAtk_7NMEfdbx",
+          name: "Wizard King (Julios) Vs Licht (Patri) Full Fight",
+          img: "https://i.ytimg.com/vi/H5qupR6YQpQ/hqdefault.jpg",
+          index:5
+        },
+        {
+          url: "https://www.youtube.com/embed/ICt1jYfucec?si=bd1cfgu_l5AI4ab9",
+          name: "Vinland Saga | SHATTERED",
+          img: "https://i.ytimg.com/vi/ICt1jYfucec/hqdefault.jpg",
+          index:6
+
+        },
+        {
+          url: "https://www.youtube.com/embed/CkC5CtabT1Y?si=KANDaR0hfcUebBsv",
+          name: "Konan Arranges",
+          img: "https://i.ytimg.com/vi/CkC5CtabT1Y/hqdefault.jpg",
+          index: 7
+        },
       
       ]
 
@@ -14,11 +56,21 @@ export default {
 
 <template>
   <div class="base">
-    <div class="container">
+    <div class="container d-flex">
       <div class="current-video">
         <iframe src="https://www.youtube.com/embed/9rYkZlt2hHE?si=3dtJ99zCLUHm3fv6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>      </div>
       <div class="playlist-video">
-
+        <div class="info-playlist">
+          video playlist 
+          <p>1/7</p>
+        </div>
+        <ul>
+          <li class="d-flex align-items-center p-2 gap-3" v-for="video in videos">
+            <p class="text-center">{{video.index}}</p>
+            <img :src="video.img" :alt="video.name">
+            <p>{{video.name}}</p>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -31,6 +83,40 @@ export default {
 }
 iframe{
   width: 840px;
+  height: 550px;
+}
+ul{
+  list-style-type: none;
+  overflow: auto;
+  height: 470px;
+  padding: 0;
+  max-width: 400px;
+}
+li{
+  border: 1px solid lightgray;
+}
+ul img{
+  width: 85px;
+  height: 65px;
+  border-radius: 10px;
+}
+li p:first-child{
+  min-height: 24px;
+  min-width: 24px;
+  color: #fff;
+  background-color: #000;
+  line-height: 24px;
+  border-radius: 5px;
+}
+.info-playlist{
+  color: white;
+  background-color: #333;
+  height: 80px;
+}
+p{
+  margin: 0;
+}
+.playlist-video{
   height: 550px;
 }
 </style>
