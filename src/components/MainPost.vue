145a31bfc6faa2a63f8b6f32ec60dd6f102dfc47
DanieleChecchin <danielechecchin26@gmail.com>
2024-09-26 14:10:32 +0200

    layout card

--- src/components/MainPost.vue
+++ src/components/MainPost.vue
@@ -1,116 +1,164 @@
 <script>
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
 
 export default {
-  data() {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
     return {
-      activeIndex: 0,
+      modules: [Pagination, Navigation],
       cardList: [
         {
           title: 'Live Ideas You Might Be Anime',
-          image: '/src/assets/img/ideas-anime.webp'
+          image: '/src/assets/img/ideas-anime.webp',
+          date: 'December 26, 2022'
         },
         {
           title: 'Reasons To Visit France',
-          image: '/src/assets/img/visit-france.webp'
+          image: '/src/assets/img/visit-france.webp',
+          date: 'December 26, 2022'
         },
         {
           title: 'Traveling Alone Is Awesome',
-          image: '/src/assets/img/travel-alone-300x200.webp'
+          image: '/src/assets/img/travel-alone-300x200.webp',
+          date: 'December 26, 2022'
         },
         {
           title: 'The Best Success Stories',
-          image: '/src/assets/img/success-story.webp'
+          image: '/src/assets/img/success-story.webp',
+          date: 'December 26, 2022'
         },
         {
           title: 'Places For A Road Trip',
-          image: '/src/assets/img/best-places-300x200.webp'
+          image: '/src/assets/img/best-places-300x200.webp',
+          date: 'December 25, 2022'
         },
         {
           title: 'Music The Love Of My Life',
-          image: '/src/assets/img/music-love.webp'
+          image: '/src/assets/img/music-love.webp',
+          date: 'December 25, 2022'
         },
         {
           title: 'Fashion Trend Now A Days',
-          image: '/src/assets/img/anime-fashion.webp'
+          image: '/src/assets/img/anime-fashion.webp',
+          date: 'December 25, 2022'
         },
         {
           title: 'The Best Winter Outfits',
-          image: '/src/assets/img/winter.webp'
+          image: '/src/assets/img/winter.webp',
+          date: 'December 26, 2022'
         },
       ]
-    }
-  }
-}
+    };
+  },
+
+};
 </script>
 
 <template>
-  <section class="container py-5">
-    <div class="top-post d-flex justify-content-between">
-      <h3>FEATURED POSTS</h3>
-      <div class="change d-flex gap-3">
-        <i class="bi bi-caret-left-square-fill"></i>
-        <i class="bi bi-caret-right-square-fill"></i>
-      </div>
-    </div>
-    <div class="py-4 text-center">
-      <div class="row">
-        <div class="col-4">
-          <div class="card">
-            <img :src='cardList[0].image' class="card-img-top" alt="immagine">
-            <div class="card-body">
-              <h5 class="card-title"> {{ cardList[0].title }} </h5>
-              <h6>December 26,2022</h6>
-              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-              <a href="#" class="btn btn-primary">Read More</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="card">
-            <img :src='cardList[1].image' class="card-img-top" alt="immagine">
-            <div class="card-body">
-              <h5 class="card-title"> {{ cardList[1].title }} </h5>
-              <h6>December 26,2022</h6>
-              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-              <a href="#" class="btn btn-primary">Read More</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="card">
-            <img :src='cardList[2].image' class="card-img-top" alt="immagine">
-            <div class="card-body">
-              <h5 class="card-title"> {{ cardList[2].title }} </h5>
-              <h6>December 26,2022</h6>
-              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-              <a href="#" class="btn btn-primary">Read More</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  <div class="container">
+    <swiper ref="{swiperRef}" :slidesPerView="3" :loop="true" :centeredSlides="false" :spaceBetween="30"
+      :navigation="true" :modules="modules" class="mySwiper">
+      <swiper-slide class="card" v-for="(card, index) in cardList" :key="index">
+        <img :src="card.image" class="card-img-top" alt="">
+        <p class="mt-3 mb-0 title"> {{ card.title }} </p>
+        <p class="date"> {{ card.date }} </p>
+        <p class="lorem"> Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+      </swiper-slide>
+    </swiper>
+    <p class="append-buttons"></p>
+  </div>
 </template>
 
-<style lang="scss" scoped>
-.card-body {
-  background-color: #F3F3F3;
+<style scoped>
+#app {
+  height: 100%;
 }
 
-h5 {
-  font-size: 19px;
+html,
+body {
+  position: relative;
+  height: 100%;
 }
 
-h6 {
-  color: #757575
+body {
+  background: #eee;
+  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  color: #000;
+  margin: 0;
+  padding: 0;
 }
 
-.btn {
-  background-color: #BF1D2F;
-  padding: 2px 30px;
-  border-radius: 15px;
-  border: 0;
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+
+  /* Center slide text vertically */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.swiper {
+  width: 100%;
+  height: 300px;
+  margin: 20px auto;
+}
+
+.append-buttons {
+  text-align: center;
+  margin-top: 20px;
+}
+
+.append-buttons button {
+  display: inline-block;
+  cursor: pointer;
+  border: 1px solid #007aff;
+  color: #007aff;
+  text-decoration: none;
+  padding: 4px 10px;
+  border-radius: 4px;
+  margin: 0 10px;
+  font-size: 13px;
+}
+
+.lorem {
+  font-size: 12px;
+}
+
+.title {
+  font-size: 14px;
+}
+
+.date {
+  font-size: 13px
 }
 </style>
