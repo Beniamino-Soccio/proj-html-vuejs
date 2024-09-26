a63b8f73f82cb0a4b435d3a05641b56a69db89d3
FrancescoMinichiello <fraps2278@gmail.com>
2024-09-26 10:59:45 +0200

    Add backround image in MainNews

--- src/components/MainNews.vue
+++ src/components/MainNews.vue
@@ -9,15 +9,31 @@ export default {
 </script>
 
 <template>
-  <h1>MainNews</h1>
+  <!--Main news-->
+  <div class="container mt-5">
+    <div class="row">
+      <h3>LIFESTYLE & STORIES</h3>
+    </div>
+    <div class="row">
+      <div class="col box-left">
+      </div>
+      <div class="col">
+      </div>
+    </div>
+  </div>
 <div>
-news
 </div>
 </template>
 
-<style scoped>
-div{
-  height: 500px;
-  border: 1px dashed goldenrod;
+<style lang="scss" scoped>
+@use 'bootstrap/scss/bootstrap.scss' as *;
+h3{
+  font-weight: bold;
+}
+.box-left{
+  background-image: url("/src/assets/img/music-love.webp");
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 500px
 }
 </style>
