973406e036189b1fe7e6b5266030e4dd1c404de0
Beniamino-Soccio <beniamino43@gmail.com>
2024-09-26 16:18:10 +0200

    hotfix trailers section + added banner on page

--- src/components/MainTrailer.vue
+++ src/components/MainTrailer.vue
@@ -62,8 +62,7 @@ export default {
 </script>
 
 <template>
-  <div class="base">
-    <div class="container d-flex">
+    <div class="container d-flex trailers">
       <div class="current-video">
         <iframe :src="activeVideo.url" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>      
       </div>
@@ -84,14 +83,9 @@ export default {
         </ul>
       </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
-.base{
-  height: 650px;
-  border: 1px dashed #ff00c8;
-}
 iframe{
   width: 840px;
   height: 550px;
@@ -139,5 +133,8 @@ li:hover{
 }
 li:hover p:first-child{
   background-color: red;
+}
+.trailers{
+  margin: 50px auto;
 }
 </style>
