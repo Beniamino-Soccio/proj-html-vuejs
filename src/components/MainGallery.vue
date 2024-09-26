bcb12b0211253b2e36f5e770b6a4b8908ae64c16
abdicore <abdirhamanmohamed@gmail.com>
2024-09-26 10:59:15 +0200

    button

--- src/components/MainGallery.vue
+++ src/components/MainGallery.vue
@@ -16,7 +16,14 @@ export default {
         <div class="ld"></div>
       </div>
       <div class="centrale">
-        <div class="c"></div>
+        <div class="c">
+          <div class="bottoni">
+            <button class="btn btn-light">Lfestyle</button>
+            <button class="btn btn-light">Stories</button>
+            <button class="btn btn-light">Travel</button>
+          </div>
+
+        </div>
       </div>
       <div class="right-side">
         <div class="ru"></div>
@@ -37,15 +44,18 @@ export default {
 }
 
 .c {
+  background-image: url(../assets/img/best-places.webp);
+  background-size: cover;
   border-radius: 10px;
-  background-color: gray;
   height: 100%;
-  width: 680px;
+  width: 675px;
   margin-left: 10px;
   margin-right: 10px;
+  position: relative;
 }
 .lu{
- 
+  background-image: url(../assets/img/anime-fashion.webp);
+  background-size: cover;
   border-radius: 10px;
   background-color: green;
   height: calc(100% / 2 - 5px);
@@ -53,7 +63,8 @@ export default {
   margin-bottom: 10px;
 }
 .ld{
- 
+  background-image: url(../assets/img/travel-alone.webp);
+  background-size: cover;
   border-radius: 10px;
   background-color: green;
   height: calc(100% / 2 - 5px);
@@ -61,6 +72,8 @@ export default {
 
 }
 .ru {
+  background-image: url(../assets/img/music-love.webp);
+  background-size: cover;
   border-radius: 10px;
   background-color: green;
   height: calc(100% / 2 - 5px);
@@ -68,15 +81,19 @@ export default {
   margin-bottom: 10px;
 
 }
-.rd{
-
+.rd{  
+  background-image: url(../assets/img/visit-france.webp);
+  background-size: cover;
   border-radius: 10px;
   background-color: green;
   width: 100%;
   height: calc(100% / 2 - 5px);
   width: 300px;
-
 }
+
+
+
+
 
 
 
