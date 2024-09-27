8f331e3f52b0b70e699f2ed59f7e2b5037d7985f
abdicore <abdirhamanmohamed@gmail.com>
2024-09-27 11:56:31 +0200

    text-data

--- src/components/MainGallery.vue
+++ src/components/MainGallery.vue
@@ -2,7 +2,7 @@
 export default {
   data() {
     return {
-      messages:[
+      cards:[
         {
           nome:'demo',
           data:'December25,2022',
@@ -36,43 +36,65 @@ export default {
   <div class="container">
     <div class="gallery">
       <div class="left-side">
-        <div class="lu">
-          <div class="bottoni">
-            <button class="btn btn-light me-2">Lifestyle</button>
-            <button class="btn btn-light me-2">Fashion</button>
+        <div class="boxLup">
+          <div class="data align-items-center ">
+            <div class="d-flex">
+              <p><i class="bi bi-person-fill me-2"></i>{{ cards[0].nome }}</p><p class="ms-2"><i class="bi bi-calendar2-fill me-2 ms-2"></i>{{ cards[0].data }}</p>
+            </div>
+            <div>
+              <h6 class="fw-bold">{{ cards[0].text }}</h6>
+            </div>
           </div>
+        
         </div>
-        <div class="ld">
-          <div class="bottoni">
-            <button class="btn btn-light me-2">Stories</button>
-            <button class="btn btn-light me-2">Travel</button>
+        <div class="boxLdown">
+          <div class="data align-items-center ">
+            <div class="d-flex">
+              <p><i class="bi bi-person-fill me-2"></i>{{ cards[1].nome }}</p><p class="ms-2"><i class="bi bi-calendar2-fill me-2 ms-2"></i>{{ cards[1].data }}</p>
+            </div>
+            <div>
+              <h6 class="fw-bold">{{ cards[1].text }}</h6>
+            </div>
           </div>
+          
         </div>
       </div>
       <div class="centrale">
-        <div class="c">
-          <div class="bottoni">
-            <button class="btn btn-light me-2">Lifestyle</button>
-            <button class="btn btn-light me-2">Stories</button>
-            <button class="btn btn-light">Travel</button>
+        <div class="boxCentrale">
+          <div class="data align-items-center ">
+            <div class="d-flex">
+              <p><i class="bi bi-person-fill me-2"></i>{{ cards[2].nome }}</p><p class="ms-2"><i class="bi bi-calendar2-fill me-2 ms-2"></i>{{ cards[2].data }}</p>
+            </div>
+            <div>
+              <h6 class="fw-bold">{{ cards[2].text }}</h6>
+            </div>
           </div>
 
         </div>
       </div>
       <div class="right-side">
-        <div class="ru">
-          <div class="bottoni">
-            <button class="btn btn-light me-2">Culture</button>
-            <button class="btn btn-light me-2">Lifestyle</button>
+        <div class="boxRup">
+          <div class="data align-items-center ">
+            <div class="d-flex">
+              <p><i class="bi bi-person-fill me-2"></i>{{ cards[3].nome }}</p><p class="ms-2"><i class="bi bi-calendar2-fill me-2 ms-2"></i>{{ cards[3].data }}</p>
+            </div>
+            <div>
+              <h6 class="fw-bold">{{ cards[3].text }}</h6>
+            </div>
           </div>
         </div>
-        <div class="rd">
-          <div class="bottoni">
-            <button class="btn btn-light me-2">Lifestyle</button>
-            <button class="btn btn-light me-2">Travel</button>
+        <div class="boxRdown">
+          <div class="data align-items-center ">
+            <div class="d-flex">
+              <p><i class="bi bi-person-fill me-2"></i>{{ cards[4].nome }}</p><p class="ms-2"><i class="bi bi-calendar2-fill me-2 ms-2"></i>{{ cards[4].data }}</p>
+            </div>
+            <div>
+              <h6 class="fw-bold">{{ cards[4].text }}</h6>
+            </div>
           </div>
         </div>
       </div>
+
       
      
     </div>
@@ -81,13 +103,15 @@ export default {
 </template>
 
 <style scoped>
-.gallery{
+
+.gallery {
   height: 500px;
   border: 1px dashed dodgerblue;
   display: flex;
+  justify-content: space-between;
 }
 
-.c {
+.boxCentrale {
   background-image: url(../assets/img/best-places.webp);
   background-size: cover;
   border-radius: 10px;
@@ -97,7 +121,7 @@ export default {
   margin-right: 10px;
   position: relative;
 }
-.lu{
+.boxLup{
   background-image: url(../assets/img/anime-fashion.webp);
   background-size: cover;
   border-radius: 10px;
@@ -108,7 +132,7 @@ export default {
   position: relative;
 
 }
-.ld{
+.boxLdown{
   background-image: url(../assets/img/travel-alone.webp);
   background-size: cover;
   border-radius: 10px;
@@ -119,7 +143,7 @@ export default {
 
 
 }
-.ru {
+.boxRup {
   background-image: url(../assets/img/music-love.webp);
   background-size: cover;
   border-radius: 10px;
@@ -131,7 +155,7 @@ export default {
 
 
 }
-.rd{  
+.boxRdown{  
   background-image: url(../assets/img/visit-france.webp);
   background-size: cover;
   border-radius: 10px;
@@ -143,17 +167,51 @@ export default {
 
 }
 
-.bottoni{
+
+.boxCentrale, .boxLup, .boxLdown, .boxRup, .boxRdown {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;  
+  align-items: center;      
+  background-size: cover;
+  border-radius: 10px;
+  position: relative;
+  color: white;
+  padding: 15px;
+  text-align: center;
+  height: 100%;
+  width: 100%;
+}
+
+.data {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;  
+  align-items: center;      
   position: absolute;
-  top: 20px;
-  left: 10px;
+  bottom: 0;                
+  left: 50%;
+  transform: translateX(-50%); 
+  width: 100%;
+  color: white;
+}
+
+p, h6 {
+  margin: 5px 0;
+  text-align: center;
 }
 
 
 
 
+.boxLup, .boxLdown, .boxRup, .boxRdown {
+  width: 300px;
+  height: calc(50% - 5px);
+  margin-bottom: 10px;
+}
 
-
-
-
+.boxCentrale {
+  width: 675px;
+  margin: 0 10px;
+}
 </style>
