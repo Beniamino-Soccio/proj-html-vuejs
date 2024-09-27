8b5233f3382dca7d8fd8d2e3a5da18f276309bcf
FrancescoMinichiello <fraps2278@gmail.com>
2024-09-27 11:40:20 +0200

    created a new component to split the header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,7 @@
 <script>
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import NewUpdatesHeaderTop from './NewUpdatesHeaderTop.vue';
 export default {
   data() {
     return {
@@ -30,72 +31,17 @@ export default {
         url: ''
        },
     ],
-    currentIndex: 0,
-    newUpdates:[
-      {
-        img:'/src/assets/img/healthy-foods-150x150.webp',
-        hours: '05.32',
-        text: 'THE BEST HEALTHY FOODS'
-      },
-      {
-        img:'/src/assets/img/rice-ball-150x150.webp',
-        hours: '05.37',
-        text: 'HYGENIC RECEIPE TO PREPARE RICE'
-      },
-      {
-        img:'/src/assets/img/winter-150x150.webp',
-        hours: '05.29',
-        text: 'THE BEST WINTER OUTFITS'
-      },
-      {
-        img:'/src/assets/img/meal-time-150x150.webp',
-        hours: '05.35',
-        text: 'THE BEST TIME TO HAVE A MEAL'
-      },
-    ]
   }
-    },
-    methods:{
-      nextUpdate(){
-        this.currentIndex = (this.currentIndex + 1) % this.newUpdates.length;
-      },
-      prevUpdate(){
-        if (this.currentIndex > 0) {
-        this.currentIndex--;
-      } else {
-        this.currentIndex = this.newUpdates.length - 1;
-      }
-      }
-    }
+},
+components: {
+  NewUpdatesHeaderTop
+}
   }
 </script>
 
 <template>
   <header>
-    <div class="news-updates d-flex align-items-center py-2">
-      <div class="container">
-        <div class="row">
-          <div class="col-2 updates-text d-flex justify-content-center align-items-center py-2">
-            <p class="m-0">NEW UPDATES</p>
-          </div>
-          <!--Current New Updates-->
-          <div class="current-new-updates col-7 d-flex align-items-center p-0 gap-3 text-white fw-bold">
-            <img :src="newUpdates[currentIndex].img" :alt="newUpdates[currentIndex].text">
-            <div><span>{{ newUpdates[currentIndex].hours }}</span></div>
-            <div><span>{{ newUpdates[currentIndex].text }}</span></div>
-          </div>
-          <!--Social Icon-->
-          <div class="social-icons col-3 d-flex justify-content-end align-items-center">
-            <div @click="prevUpdate"><i class="bi bi-arrow-bar-left"></i></div>
-            <div @click="nextUpdate"><i class="bi bi-arrow-bar-right me-5"></i></div>
-            <a href="#"><i class="bi bi-facebook"></i></a>
-            <a href="#"><i class="bi bi-twitter-x"></i></a>
-            <a href="#"><i class="bi bi-instagram"></i></a>
-            <a href="#"><i class="bi bi-youtube"></i></a>
-          </div>
-        </div>
-      </div>
-    </div>
+    <NewUpdatesHeaderTop/>
     <div class="container my-3">
       <div class="row">
         <!--logo header-->
@@ -131,30 +77,6 @@ export default {
 <style lang="scss" scoped>
 @use 'bootstrap/scss/bootstrap.scss' as *;
 
-.bi-arrow-bar-left, .bi-arrow-bar-right{
-  cursor: pointer;
-}
-
-.current-new-updates{
-  height: 50px;
-}
-
-.current-new-updates img{
-  height: 100%;
-}
-
-.updates-text{
-  background-color: #545454;
-  color: white;
-  font-weight: bold;
-  height: 50px;
-}
-
-.news-updates{
-  background-color: #BF1D2E;
-  height: 50px;
-}
-
 a{
   text-decoration: none;
   color: black;
@@ -182,14 +104,5 @@ li{
 
 a.nav-link:hover{
   color: #BF1D2E
-}
-
-.social-icons i{
-  background-color: white;
-  border-radius: 50%;
-  padding: 5px 8px;
-  color: #BF1D2E;
-  font-size: 15px;
-  margin-right: 10px;
 }
 </style>
