d4324dcc510a4f5a24ca51302a8ad09ef9369a36
FrancescoMinichiello <fraps2278@gmail.com>
2024-09-27 16:51:15 +0200

    Fixed MainNews - v 1.0

--- src/components/MainNews.vue
+++ src/components/MainNews.vue
@@ -79,13 +79,16 @@ export default {
 
 <template>
   <div class="container mt-5">
-    <div class="row">
-      <h3 class="p-0">LIFESTYLE & STORIES</h3>      
-      <div class="category-buttons my-3">
-        <button class="btn category-btn" :class="{ active: selectedCategory === 'ALL' }" @click="changeCategory('ALL')">ALL</button>
-        <button class="btn category-btn" :class="{ active: selectedCategory === 'LIFESTYLE' }" @click="changeCategory('LIFESTYLE')">LIFESTYLE</button>
-        <button class="btn category-btn" :class="{ active: selectedCategory === 'STORIES' }" @click="changeCategory('STORIES')">STORIES</button>
-      </div>
+
+    <div class="d-flex justify-content-between align-items-center mb-5">
+        <h3 class="p-0 fw-semibold">LIFESTYLE & STORIES</h3>      
+        <div class="category-buttons">
+
+          <button class="btn category-btn" :class="{ active: selectedCategory === 'ALL' }" @click="changeCategory('ALL')">ALL</button>
+          <button class="btn category-btn" :class="{ active: selectedCategory === 'LIFESTYLE' }" @click="changeCategory('LIFESTYLE')">LIFESTYLE</button>
+          <button class="btn category-btn" :class="{ active: selectedCategory === 'STORIES' }" @click="changeCategory('STORIES')">STORIES</button>
+
+        </div>
     </div>
 
     <div class="custom-loader-mask" v-if="isLoading">
@@ -93,9 +96,9 @@ export default {
     </div>
 
     <div class="row box position-relative"> 
-      <div class="col-lg-7 box-left ps-0">
+      <div class="col-7 box-left pe-0">
         <a href="#">
-          <img :src="mainImage.img" :alt="mainImage.title" class="main-img">
+          <img :src="mainImage.img" :alt="mainImage.title">
           <span class="badge-card main-badge">{{ mainImage.badge }}</span>
         </a>
         <div v-if="!isLoading" class="main-info text-white">
@@ -110,7 +113,7 @@ export default {
       <div class="col-lg-5 d-flex flex-column">
         <div class="lifestyle-stories-card d-flex align-items-center gap-3" v-for="(card, index) in rightSideCards" :key="index">
           <a href="#"><img class="img-card me-3" :src="card.img" :alt="card.title"></a>
-          <div class="info-card text-secondary">
+          <div class="info-card text-secondary fw-semibold">
             <span class="badge-card">{{ card.badge }}</span>
             <p><i class="bi bi-person-fill"></i> {{ card.author }} <i class="bi bi-calendar2-fill"></i> {{ card.date }}</p>
             <p>{{ card.title }}</p>
@@ -124,6 +127,18 @@ export default {
 
 <style lang="scss" scoped>
 @use 'bootstrap/scss/bootstrap.scss' as *;
+
+.info-card{
+  font-weight: bold;
+  span{
+    color: #545454;
+    cursor: pointer;
+  }
+  &
+  :hover{
+    color: #BF1D2E;
+  }
+}
 
 .custom-loader-mask {
   background: rgba(54, 54, 54, 0.8);
@@ -164,10 +179,6 @@ export default {
   }
 }
 
-h3 {
-  font-weight: bold;
-}
-
 .box-left {
   position: relative; 
 }
@@ -188,6 +199,8 @@ h3 {
   width: 100%;
   cursor: pointer;
   filter: brightness(50%); 
+  text-align: end;
+  object-fit: contain;
 }
 
 .img-card {
@@ -209,6 +222,10 @@ h3 {
   cursor: pointer;
 }
 
+.box-left .badge-card{
+  left: 50px;
+}
+
 .text-white {
   color: white; 
 }
@@ -221,16 +238,13 @@ h3 {
   margin-top: 10px;
     position: relative;
     top: -75px;
-    left: 10px;
+    left: 35px;
 }
 
-.info-card {
-  font-weight: bold;
   span {
     color: #545454;
     cursor: pointer;
   }
-}
 
 .lifestyle-stories-card {
   margin-bottom: 25px;
@@ -251,7 +265,6 @@ p {
     margin-bottom: 1rem !important;
     position: relative;
     top: -58px;
-    left: 850px;
 }
 
 .row {
@@ -269,24 +282,23 @@ p {
 }
 
 .category-btn {
-  border: 2px solid grey;
-  background-color: white;
-  color: grey;
+  color: white;
   padding: 5px 15px;
   font-weight: bold;
   cursor: pointer;
   transition: all 0.3s ease;
+  background-color: #333;
 }
 
 .category-btn:hover {
-  color: red;
-  border-color: red;
+  background-color: #BF1D2E;
+  color: white;
 }
 
 .category-btn.active {
   color: white;
-  background-color: red;
-  border-color: red;
+  background-color: #BF1D2E;
+  border-color: #BF1D2E;
 }
 
 .main-info-text {
@@ -294,4 +306,154 @@ p {
   color: #ffffff; 
   margin-bottom: 5px;
 }
+
+img{
+  filter: brightness(50%);
+}
+img:hover{
+  filter: brightness(60%)
+}
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--
+  
+
+<script>
+export default {
+  data() {
+    return {
+      lifestyleStoriesCard: [
+      {
+        img: '/src/assets/img/anime-fashion.webp',
+        author: 'demo',
+        date: 'December 25, 2022',
+        title: 'Fashion trend now a days',
+        badge: 'Fashion'
+      },
+      {
+        img: '/src/assets/img/best-places-300x200.webp',
+        author: 'demo',
+        date: 'December 25, 2022',
+        title: 'Places for a road trip',
+        badge: 'Lifestyle'
+      },
+      {
+        img: '/src/assets/img/ideas-anime.webp',
+        author: 'demo',
+        date: 'December 25, 2022',
+        title: 'Live Ideas you might be anime',
+        badge: 'Culture'
+      }
+      ]
+    }
+  }
+}
+</script>
+<template>
+  <div class="container mt-5">
+    <div class="row">
+      <h3 class="p-0">LIFESTYLE & STORIES</h3>
+    </div>
+    <div class="row box">
+      <div class="col box-left ps-0">
+        <a href="#"><img src="/src/assets/img/music-love.webp" alt="Music Love"></a>
+      </div>
+      <div class="col d-flex flex-column">
+    
+      <div class="lifestyle-stories-card d-flex align-items-center gap-4" v-for="(card, index) in lifestyleStoriesCard" :key="index">
+  
+        <a href="#"><img class="img-card me-3" :src="card.img" :alt="card.title"></a>
+        
+        <p class="badge-card">{{ card.badge }}</p>
+      
+        <div class="info-card">
+          <span class="me-3"><i class="bi bi-person-fill"></i> {{ card.author }}</span>
+          <span><i class="bi bi-calendar2-fill"></i> {{ card.date }}</span>
+          <p>{{ card.title }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div>
+</div>
+</template>
+<style lang="scss" scoped>
+@use 'bootstrap/scss/bootstrap.scss' as *;
+h3{
+  font-weight: bold;
+}
+.box-left img{
+  height: 500px;
+  width: 100%;
+  cursor: pointer;
+}
+
+.badge-card{
+  position: absolute;
+  top: 15px;
+  left: 15px;
+  background-color: white;
+  border-radius: 5px;
+  padding: 2px 15px;
+  font-weight: bold;
+  cursor: pointer;
+}
+.badge-card:hover{
+  color: #BF1D2E;
+}
+.bi{
+  font-size: 20px;
+}
+.box{
+  height: 500px;
+}
+.info-card{
+  font-weight: bold;
+  span{
+    color: #545454;
+    cursor: pointer;
+  }
+  &
+  :hover{
+    color: #BF1D2E;
+  }
+}
+.lifestyle-stories-card{
+  margin-bottom: 25px;
+  border-bottom: 1px solid black;
+  position: relative;
+}
+.lifestyle-stories-card:last-child{
+  border-color: transparent;
+}
+img{
+  filter: brightness(50%);
+}
+img:hover{
+  filter: brightness(60%)
+}
+p{
+  cursor: pointer;
+}
+</style>
+-->
